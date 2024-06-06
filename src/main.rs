53809b6845fc4e3a8dfb0ecd27679e8cfe8a9d0d
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-05 21:28:18 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod utils;
+use utils::terminal::{limpar_tela, esperar_enter};
+fn main() {
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    esperar_enter();
+    limpar_tela();
+    println!("Hello, world!");
+
+}
