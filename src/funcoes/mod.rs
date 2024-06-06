8769b98e1653adcc9712624393a0972dd513281b
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-05 23:22:52 -0300

    Finalizando a implementação do modulo de Funções do rust

---
+++ src/funcoes/mod.rs
@@ -0,0 +1,24 @@
+mod fns;
+mod lambda;
+
+use crate::utils::terminal::{esperar_enter, exibir_menu, limpar_tela};
+
+pub fn executar(){
+    loop {
+        let itens = ["Básicos", "Map", "Filter"];
+
+        limpar_tela();
+
+        let selecionado = exibir_menu("Funções", &itens, true);
+        limpar_tela();
+
+        match selecionado {
+            1 => fns::exemplo(),
+            2 => lambda::exemplo_map(),
+            3 => lambda::exemplo_filter(),
+            _ => break,
+        }
+        esperar_enter();
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod utils;
 mod fundamentos;
 mod tipos;
 mod controle;
+mod funcoes;
 
 use std::process::exit;
 use utils::terminal::{limpar_tela, esperar_enter, exibir_menu};
@@ -14,7 +15,7 @@ fn main() {
             1 => fundamentos::executar(),
             2 => tipos::executar(),
             3 => controle::executar(),
-            4 => println!("4"),
+            4 => funcoes::executar(),
             5 => println!("5"),
             _ => exit(0)
         }
